dc414d83b5c59f7c0287e9fce0026258dbf92876
adaisbae <melissajoneswrites@gmail.com>
2019-08-15 22:19:54 -0400

    completed challenge 5. made an array of top donors, an email list and picked a random contest winner

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -76,14 +76,23 @@ console.log(largeShirts);
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 let ticketPriceTotal = [];
-ticketPriceTotal.push(runners.reduce((acc, runners) => { return acc + runners.donation},0));
+ticketPriceTotal.push(runners.reduce((acc, runners) => {return acc + runners.donation},0));
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
+let topDonors= [];
+topDonors.push(runners.filter(runners => runners.donation > 100));
+console.log(topDonors);
 
 // Problem 2
+let emailList = [];
+emailList.push(runners.map(runners => `${runners.email}`));
+console.log(emailList);
 
 // Problem 3
+let randomWinner = [];
+randomWinner.push(runners[Math.floor(Math.random() * runners.length)]);
+console.log(randomWinner);
